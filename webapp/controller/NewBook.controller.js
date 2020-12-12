5d204c9467b0a0f66c8c02ad036d8a1995ec3b4f
GuilSchaidhauer <e.guilherme.castro@gmail.com>
2020-12-12 18:37:49 -0300

    Add NewBook page

---
+++ webapp/controller/NewBook.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel", 
+], function(Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.todo.controller.NewBook", {
+
+		onInit: function() {
+			this.oModel = new JSONModel({});
+
+			this.getView().setModel(this.oModel);
+		},
+
+		_refreshModel: function() {
+			this.getView().setModel(this.oModel);
+			this.getView().getModel().refresh();
+		}
+
+	});
+
+});
